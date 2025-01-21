4e75cc5956fa1bf5886ea1b7755e8617fc27f9c0
brc0d3s <brc0d3s@gmail.com>
2025-01-21 12:18:31 +0630

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="images/logo.ico" rel="icon" type="image/x-icon" />
+	<link href="index.css" rel="stylesheet" type="text/css"/>
+	<link href="css/bar/bar.css" rel="stylesheet" type="text/css"/>
+	<link href="css/dark/dark.css" rel="stylesheet" type="text/css"/>
+    <link href="css/default/default.css" rel="stylesheet" type="text/css"/>
+	<link href="css/light/light.css" rel="stylesheet" type="text/css"/>
+	<link href="css/nivo-slider.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+    
+</body>
+</html>
