27b03b933be3de710712a07ba630375acbb93ec0
brc0d3s <brc0d3s@gmail.com>
2025-01-23 23:23:47 +0300

    static web

---
+++ socialActivities.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Social Activities</title>
+    
+    <link href="img/logo.ico" rel="icon" type="image/x-icon" />
+    <link rel="css/stylesheet" href="navBar.css">
+    <link rel="css/stylesheet" href="footer.css">
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+   
+</head>
+<body>
+    <div class="fix main">
+        <!-- Navigation Menu -->
+        <?php require_once 'navBar.php'; ?>
+
+        <div class="fix about">
+				<h1>Our Social Activities</h1>
+			
+				<table class="listTable" border="2px" cellpadding="0px" cellspacing="0px">
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+					
+					<tr>
+						<td bgcolor="#CCEEBC">
+							<h3>We are mainly doing social activities, one of the voluntary donation of blood.
+							We can find blood donors through various campaigns, we have the best efforts to provide blood donation at any time to the needs of the patient's blood, and we organize quick quiz on various national days and hunt talent.</h3>
+						</td>
+						<td colspan="2" class="list1" bgcolor="#FBAFC1">
+							<ul>
+								<li>
+									<p title="Click here to watch Photo"><a href="#one">Winter clothes among cold-witted people.</a></p>
+								</li>
+								<li>
+									<p title="Click here to watch Photo"><a href="#two">Special Quick Quiz Competition on the occasion of Language Day.</a></p>
+								</li>
+								<li>
+									<p title="Click here to watch Photo"><a href="#three">Eid clothing and Eid content distribution among underprivileged people.</a></p>
+								</li>
+								<li>
+									<p title="Click here to watch Photo"><a href="#four">Iftar prayers and al-Qur'an eradication with the underprivileged people.</a></p>
+								</li>
+								<li>
+									<p title="Click here to watch Photo"><a href="#five">Everyone in the rush of victory (Free Blood Grouping Camp).</a></p>
+								</li>
+								<li>
+									<p title="Click here to watch Photo"><a href="#six">Motivation team to search for talent.</a></p>
+								</li>
+								<li>
+									<p title="Click here to watch Photo"><a href="#seven">Eid joy and public awareness cultural programs with underprivileged people.</a></p>
+								</li>
+								<li>
+									<p title="Click here to watch Photo"><a href="#eight">Donating blood for emergency patients.</a></p>
+								</li>
+							</ul>
+						</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</table>
+			</div>
+
+
+
+			<div class="fix div1">
+
+				<div class="bottomImage">	
+					<div class="imageLeft">
+						<a href="#" class="tooltip"><img src="images/winter.png"/><span>Blood Campaign</span></a>
+					</div>
+					<div class="imageRight">
+						<a href="#" class="tooltip"><img src="images/quiz.png"/><span>Eid Content Distribution</span></a>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="fix div1">
+
+				<div class="bottomImage">	
+					<div class="imageLeft">
+						<a href="#" class="tooltip"><img src="images/eid.png"/><span>Helping Poor People</span></a>
+					</div>
+					<div class="imageRight">
+						<a href="#" class="tooltip"><img src="images/iftar.png"/><span>Blood Group Checking</span></a>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="fix div1">
+
+				<div class="bottomImage">	
+					<div class="imageLeft">
+						<a href="#" class="tooltip"><img src="images/bloodchecking.png"/><span>Blood Campaign</span></a>
+					</div>
+					<div class="imageRight">
+						<a href="#" class="tooltip"><img src="images/talent.png"/><span>Eid Content Distribution</span></a>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="fix div1">
+
+				<div class="bottomImage">	
+					<div class="imageLeft">
+						<a href="#" class="tooltip"><img src="images/eidfood.png"/><span>Helping Poor People</span></a>
+					</div>
+					<div class="imageRight">
+						<a href="#" class="tooltip"><img src="images/blooddonation.png"/><span>Blood Group Checking</span></a>
+					</div>
+				</div>
+
+			</div>
+
+
+        <!-- Footer -->
+        <?php require_once "footer.php"; ?>
+    </div>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
--- whoCanGiveBlood.php
+++ whoCanGiveBlood.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>VitalDrops Motive</title>
+    <title>Donor Conditions</title>
     
     <link href="img/logo.ico" rel="icon" type="image/x-icon" />
     <link rel="css/stylesheet" href="navBar.css">
@@ -16,14 +16,9 @@
         <!-- Navigation Menu -->
         <?php require_once 'navBar.php'; ?>
 
-        <div class="slider-wrapper">
-            <div id="slider">
-                <img src="img/1015.jpg" alt="Slide 1" title="This site is developed by brc0d3s" />
-                <img src="img/1014.jpg" alt="Slide 2" title="This site is developed by brc0d3s" />
-                <img src="img/1012.jpg" alt="Slide 3" title="This site is developed by brc0d3s" />
-                <img src="img/1011.jpg" alt="Slide 4" title="This site is developed by brc0d3s" />
-            </div>
-        </div>
+        
+
+
         <!-- Footer -->
         <?php require_once "footer.php"; ?>
     </div>
